9548a8088ced8b7d901930b86af2a175321a51dd
ZY Sim <me@zhongyuen.dev>
2021-05-28 21:57:52 +0800

    First Commit

---
+++ Go/mineTest.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+
+func stuff() {
+anArray := make([]int, 2)
+  anArray[0] = 0
+  anArray[1] = 1
+appendedArray := append(anArray, 3, [2]int{4, 5})
+
+myMap := map[string]int{"Test": 1}
+myMap["Another"] = 2
+}
+
+
+func arrayAndMapArgs(a []int, b map[string]int) {
+  fmt.Println(a, b)
+}
+
+func sameType(a, b int) int {
+  return a + b
+}
+
+func differentTypes(a string, b int) (string, int) {
+  return a, b
+}
+
+func main() {
+    // fmt.Println(t("1", 2))
+}
